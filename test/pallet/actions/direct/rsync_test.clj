f8a431b9991ca3ed7362197371c471f2adf110b0
Ryan Stradling <ryan.stradling@fiveoneninegames.com>
2012-07-12 18:38:09 -0400

    Putting back in the rsync_test changes that I commented out and inavertently submitted

--- test/pallet/actions/direct/rsync_test.clj
+++ test/pallet/actions/direct/rsync_test.clj
@@ -18,4 +18,48 @@
 
 (use-fixtures :once (logging-threshold-fixture))
 
+(deftest rsync-test
+  (with-admin-user (assoc *admin-user*
+                     :username (test-username))
+    (utils/with-temporary [dir (utils/tmpdir)
+                           tmp (utils/tmpfile dir)
+                           target-dir (utils/tmpdir)]
+      ;; this is convoluted to get around the "t" sticky bit on temp dirs
+      (let [user (assoc *admin-user*
+                   :username (test-username) :no-sudo true)
+            compute (make-localhost-compute :group-name "tag")
+            tag (group-spec "tag" :packager :no-packages)]
+        (io/copy "text" tmp)
+        (.delete target-dir)
+
+        (let [op (lift
+                  tag
+                  :compute compute
+                  :phase (rsync (.getPath dir) (.getPath target-dir) {})
+                  :environment {:user user})
+              target-tmp (java.io.File.
+                          (str (.getPath target-dir)
+                               "/" (.getName dir)
+                               "/" (.getName tmp)))]
+          @op
+          (is (complete? op))
+          (is (.canRead target-tmp))
+          (is (= "text" (slurp (.getPath target-tmp))))
+          (.delete target-tmp))
+        (.delete target-dir)
+        (let [op (lift
+                   tag
+                   :compute compute
+                   :phase (rsync-directory (.getPath dir) (.getPath target-dir))
+                   :environment {:user user})
+              target-tmp (java.io.File.
+                          (str (.getPath target-dir)
+                               "/" (.getName dir)
+                               "/" (.getName tmp)))]
+          @op
+          (is (complete? op))
+          (is (.canRead target-tmp))
+          (is (= "text" (slurp (.getPath target-tmp))))
+          (.delete target-tmp))))))
+
 
